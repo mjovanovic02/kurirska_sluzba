220f70df04f9096b9aa8bc3860ec15e4aac38b4c
mjovanovic02 <mjovanovic2421it@raf.rs>
2026-01-14 15:59:32 +0100

    Pomocu artisana napravi kontrolere modele i promeni rute

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\KlijentController;
+use App\Http\Controllers\PosiljkaController;
+use App\Http\Controllers\KurirController;
+use App\Http\Controllers\MenadzerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +16,22 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Resource routes
+    Route::resource('klijenti', KlijentController::class);
+    Route::resource('posiljke', PosiljkaController::class);
+    Route::resource('kuriri', KurirController::class);
+    Route::resource('menadzeri', MenadzerController::class);
+
+    // Special use cases
+    Route::post('/posiljke/kreiraj', [PosiljkaController::class, 'kreiraj'])->name('posiljke.kreiraj');
+    Route::post('/posiljke/{id}/preuzmi', [PosiljkaController::class, 'preuzmi'])->name('posiljke.preuzmi');
+    Route::post('/posiljke/{id}/dostavi', [PosiljkaController::class, 'dostavi'])->name('posiljke.dostavi');
+    Route::post('/posiljke/{id}/dodeli', [PosiljkaController::class, 'dodeli'])->name('posiljke.dodeli');
 });
 
 require __DIR__.'/auth.php';
